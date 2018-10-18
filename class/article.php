baece2537c2a332b7c0c5e42e2a6bc4b706fab50
Raphael <r-e.laurence@orange.fr>
2018-10-18 14:39:51 -0400

    création des getters et des setters

--- class/article.php
+++ class/article.php
@@ -5,6 +5,37 @@ private $_titre
 private $_contenu
 private $_date
 
+// création des getters
+    
+public function getId()
+{
+ return $this->_id; 
+}
+    
+public function getTitre()
+{
+ return $this->_titre; 
+}
 
+public function getContenu()
+{
+ return $this->_contenu; 
+    
+}
 
+public function getDate()
+{
+ return $this->_date; 
+        
+}
+    
+// création des setters
+    
+public function setId($id)
+{
+  if(isset($id=int($id)&& $id>0))
+      $this->_id=$id;
+  }
+      
+    
 }
