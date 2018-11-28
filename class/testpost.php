c3fd7a62d43c3e51d1bf5b2231d199f16b9a8cb5
FAYO <r-e.laurence@orange.fr>
2018-11-27 23:45:53 -0400

    test fichier testpost.php

--- class/testpost.php
+++ class/testpost.php
@@ -2,11 +2,11 @@
 require_once('Database.php');
 require_once('PostManager.php');
 
-$test= new PostManager();
+$test= new PostManager;
 $req = $test->get_all_posts();
 //print_r($req);
 while($data = $req->fetch())
 {
-echo $data['Title'].'<br/>';
+echo '<strong>'.$data['Title'].'</strong/><br/>';
 }
     
